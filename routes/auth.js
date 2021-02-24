82efe3db129963f0241c0dd49b561c8abadd3aab
Capabay <capabay@hotmail.com>
2021-02-24 22:34:28 +0100

    first commit, working server

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const express= require('express');
+const router = express.Router();
+
+router.get('/', (req, res)=>{
+
+    res.send('hompagePost')
+});
+
+router.post('/register', (req, res)=>{
+    res.send('register')
+});
+
+router.post('/user', (req, res)=>{
+    res.send('user route')
+});
+
+router.get('/api/customers', (req, res)=>{
+    const customers = [
+        {id:1, FirstName:"Mark", LastName:"Henry"},
+        {id:2, FirstName:"sally", LastName:"Hen"},
+        {id:3, FirstName:"May", LastName:"roy"},
+        {id:4, FirstName:"Mark", LastName:"lack"}
+    ];
+     res.json(customers)
+});
+module.exports = router; 
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
+require ('dotenv/config');
+//Import Routes
+const authRoute = require('./routes/auth');
+
+
+
+
+//Route middlewares
+app.use('/', authRoute);
+app.use('/user', authRoute);
+app.use('/api/customers', authRoute);
+
+
+
+
+
+//Connect to MongoDb 
+const url = `mongodb+srv://capa_user:keneth@cluster0-shard-00-01.to8ha.mongodb.net/capa_db?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url, { useNewUrlParser: true}, ()=> console.log('connect to database')
+);
+    //Create an instance of mongodb client
+const client = new MongoClient(url);
+const port = 6000;
+// Listen on a specific host via the HOST environment variable
+var host = process.env.HOST || '0.0.0.0';
+app.listen(port, ()=> console.log(`server started on port ${port}`));
